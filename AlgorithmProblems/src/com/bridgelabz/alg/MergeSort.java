bcf1387658169e4d1c1d2ee01a0838e51b679f62
kavyavarma421 <kavya.kvr23@gmail.com>
2019-11-06 11:57:24 +0530

    new programms

--- AlgorithmProblems/src/com/bridgelabz/alg/MergeSort.java
+++ AlgorithmProblems/src/com/bridgelabz/alg/MergeSort.java
@@ -4,88 +4,88 @@ import java.util.Scanner;
 
 public class MergeSort 
 {
- int[] array;
- int length;
- int[] tempArr;
-
-public  void sort(int[] inputArr)
-{
-	this.array = inputArr;
-	this.length = inputArr.length;
-	 this.tempArr = new int[length];
-	divideArray(0, length-1);
+ 
+public static void sort(int[] array)
+{ 
+	int n = array.length; 
+     for (int i=0; i<n; ++i)
+     {
+      System.out.print(array[i] + " "); 
+     }
+     System.out.println(); 
+	 
 }
-public  void divideArray(int low, int high)
+public static  void divideArray(int[] array, int low, int high)
 {
 	if(low < high)
 	{
 		int mid = low + (high - low)/2;
 		
 		//it will sort the left side of an array
-		divideArray(low, mid);
+		divideArray(array, low, mid);
 		// it will sort the right side of an array
-		divideArray(mid+1, high);
+		divideArray(array, mid+1, high);
 		
-		mergeArray(low, mid, high);
+		mergeArray(array, low, mid, high);
 	}
 }
-public void mergeArray(int low, int mid, int high)
+public static void mergeArray(int[] array, int low, int mid, int high)
 {
  int i = low;
  int j = mid + 1;
  int k = low;
+ int[] array1 = new int[array.length];
  while(i <= mid && j <= high)
  {
-	 if(tempArr[i] <= tempArr[j])
+	 if(array[i] <= array[j])
 	 {
-		 array[k] = tempArr[i];
+		array1[k] = array[i];
 		 i++;
 	 
 	 }
 	 else
 	 {
-		 array[k] = tempArr[j];
+		 array1[k] = array[j];
 		 j++;
 		 
 	 }
 	 k++;
  }
- if(i>mid)
+ while(i <= mid)
  {
-	 while(j <= high)
-	 {
-		 array[k] = tempArr[j];
-		 j++;
-		 k++;
-		
-	 }
+	 array1[k] = array[i];
+	 k++;
+	 i++;
  }
- else
+ while(j <= high)
  {
-   while(i <= mid)
-   {
-	   array[k] = tempArr[i];
-	   i++;
-	   k++;
-	   
-   }
+	 array1[k] = array[j];
+	 j++;
+	 k++;
  }
  for( k = low;k <= high; k++)
  {
-	 tempArr[k] = array[k];
+	 array[k] = array1[k];
 	
  }
 }
 public static void main(String[] args) 
 {
-    int[] inputArr = {48, 32, 53, 24, 16, 19, 65};
-    MergeSort ms = new MergeSort();
-           ms.sort(inputArr);
-          System.out.println("the sorted order is ");
-       for(int x:inputArr)
-       {
-            System.out.println(x+" ");
-       }
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter the size of the array: ");
+	int n = sc.nextInt();
+	int[] array = new int[n];
+	System.out.println("enter the elements you want to sort: ");
+	for(int i = 0;i < array.length;i++)
+	{
+		array[i] = sc.nextInt();
+	}
+	System.out.println("given array");
+	sort(array);
+	divideArray(array, 0, array.length-1);
+	System.out.println("sorted array is: ");
+	sort(array);
+	
 }
 	
 }
