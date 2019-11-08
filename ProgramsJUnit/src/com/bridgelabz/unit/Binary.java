a4ee3fc9c7832bb969d4b790d892fdcad742e517
kavyavarma421 <kavya.kvr23@gmail.com>
2019-11-08 10:42:06 +0530

    updated

--- ProgramsJUnit/src/com/bridgelabz/unit/Binary.java
+++ ProgramsJUnit/src/com/bridgelabz/unit/Binary.java
@@ -1,8 +1,23 @@
 package com.bridgelabz.unit;
 
-
+import java.util.Scanner;
 
 public class Binary 
 {
-	                                                                                                                                                                                                                                      
+	    public static int swapNibbles(int n)
+	    {
+	    	
+			return ((n & 0x0F)<<4 | (n & 0xF0)>>4);
+	    	
+	    }
+	    public static void main(String[] args) 
+	    {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("enter the number: ");
+		 int n = sc.nextInt();
+		 int s = swapNibbles(n);
+		 System.out.println("after the swaping the no is: "+s);
+		 
+		 
+		}
 }
