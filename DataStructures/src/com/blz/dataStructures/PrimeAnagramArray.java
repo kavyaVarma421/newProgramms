1fc7000a0a912065b9f0f05d78c159c9b8d28086
kavyavarma421 <kavya.kvr23@gmail.com>
2019-11-20 16:24:48 +0530

    updated

--- DataStructures/src/com/blz/dataStructures/PrimeAnagramArray.java
+++ DataStructures/src/com/blz/dataStructures/PrimeAnagramArray.java
@@ -3,41 +3,44 @@ package com.blz.dataStructures;
 import java.util.Arrays;
 
 public class PrimeAnagramArray {
+	public static boolean anagram(int val, int[][] ary) {
+		String str2,str1;
+		str1=String.valueOf(val);
+		for (int[] k : ary) {
+			for (int l : k) {
+				str2 = String.valueOf(l);
+				if (str1.length() == str2.length() && !str1.equals(str2)) {
 
-	public static int palindrome(int n) {
-		if (n == -1) {
-			return 0;
+					char[] ch1 = str1.toCharArray();
+					Arrays.sort(ch1);
+					char[] ch2 = str2.toCharArray();
+					Arrays.sort(ch2);
+					if (String.valueOf(ch1).equals(String.valueOf(ch2))) {
+						return true;
+					}
+
+				}
+			}
 		}
-		int remainder, reversedint = 0, originalint;
-		originalint = n;
-		while (n != 0) {
-			remainder = n % 10;
-			reversedint = reversedint * 10 + remainder;
-			n = n / 10;
-		}
-		if (originalint == reversedint) {
-			System.out.print(originalint + " ");
-			return originalint;
-		}
-		return 0;
+		return false;
 	}
 
 	public static void main(String[] args) {
 		Prime2dArray agrm = new Prime2dArray();
-		int[] anagram = new int[1000];
+		int[] anagramAry = new int[1000];
 		int pos = 0;
 		agrm.primeArray();
 		for (int i = 0; i < agrm.primeNum.length; i++) {
-			for (int j = 0; j < agrm.primeNum[i].length; j++) {
-				int temp = palindrome(agrm.primeNum[i][j]);
-				if (temp != 0) {
-					anagram[pos] = temp;
+			for (int j = 0; j < agrm.primeNum[i].length; j++) 
+			{ 
+			 
+				if(anagram(agrm.primeNum[i][j],agrm.primeNum)){
+					anagramAry[pos]=agrm.primeNum[i][j];
 					++pos;
 				}
-
 			}
-		}
-		anagram = Arrays.copyOf(anagram, pos);
-		System.out.println(Arrays.toString(anagram));
-	}
+		}		
+		anagramAry = Arrays.copyOf(anagramAry, pos);
+		System.out.println(Arrays.toString(anagramAry));
+ }
 }
